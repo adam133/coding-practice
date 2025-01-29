320af60f5f4b7d6ad9ddad2093f8b66606c63398
Adam Best <abest@tractorzoom.com>
2025-01-29 15:23:44 -0600

    use latest google library

--- day4/main.py
+++ day4/main.py
@@ -1,26 +1,65 @@
+import vertexai
 
-from google.cloud import aiplatform
+from vertexai.generative_models import GenerativeModel
 from dotenv import load_dotenv
 import os
+from loguru import logger
 
 
-def get_envs():
+STORY_PROMPT = """
+Write a short (1 paragraph) adventure story that includes the following inputs as details:
+"""
+
+# get the environment variables
+def get_envs() -> dict[str, str]:
     load_dotenv()
     config = {}
     config["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+    config["PROJECT_ID"] = os.getenv("PROJECT_ID")
+    config["REGION"] = os.getenv("REGION")
     return config
 
 
+# get the story inputs from the user
+def get_input() -> dict[str, str]:
+    name = input("What is your name?")
+    worst_enemy = input("What is your worst enemy’s name?")
+    superpower = input("What is your superpower?")
+    where_do_you_live = input("Where do you live?")
+    favorite_food = input("What is your favorite food?")
+
+    all_inputs = {
+        "name": name,
+        "worst_enemy": worst_enemy,
+        "superpower": superpower,
+        "where_do_you_live": where_do_you_live,
+        "favorite_food": favorite_food,
+    }
+    return all_inputs
+
+
+# going to make this task a bit more complex by using an LLM to generate the story
 def main():
-    configs = get_envs()
+    logger.debug("Getting environment variables")
+    configs: dict[str, str] = get_envs()
 
-    vertexai.init(project=configs["GOOGLE_API_KEY"])
+    logger.debug("Getting story inputs from user")
+    inputs: dict[str, str] = get_input()
 
-    model = GenerativeModel("gemini-1.5-flash-latest")
+    logger.debug("Initializing VertexAI client")
+    vertexai.init(project=configs["PROJECT_ID"], location=configs["REGION"])
 
-    response = model.generate_content("Hello, world!")
+    logger.debug("Initializing Gemini model")
+    model = GenerativeModel("gemini-1.5-flash-002")
 
-    print(response.text)
+    inputs_str = "\n".join([f"{key}: {value}" for key, value in inputs.items()])
+
+    logger.debug("Generating story")
+    input_prompt = STORY_PROMPT + "\n" + inputs_str
+    responses = model.generate_content(input_prompt, stream=False)
+
+    logger.debug("Printing story")
+    logger.info(responses.text)
 
 
 if __name__ == "__main__":
